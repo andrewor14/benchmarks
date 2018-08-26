738c65f7e643ebf12efb2320c5cbf5ba2602ded0
andrewor14 <andrewor14@gmail.com>
2018-08-26 07:36:57 -0400

    Refactor run script to use common env setup script

--- scripts/tf_cnn_benchmarks/run_benchmark.sh
+++ scripts/tf_cnn_benchmarks/run_benchmark.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-module load cudnn/cuda-9.1/7.1.2
-module load anaconda3/5.0.1
-
-# Note: This makes sure we're running tensorflow with GPU support
-# This is commented out because we only need to do this once
-#pip3 uninstall -y tensorflow
-#pip3 uninstall -y tensorflow-gpu
-#pip3 install --user tensorflow-gpu
-
-python test_gpu_support.py
-#python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 --variable_update=parameter_server
+python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 --variable_update=parameter_server
 
