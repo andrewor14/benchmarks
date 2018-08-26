bcc9304041ac7e645ef47e14c17c19d929e2cefa
andrewor14 <andrewor14@gmail.com>
2018-08-26 07:36:57 -0400

    Refactor run script to use common env setup script

---
+++ scripts/tf_cnn_benchmarks/run_with_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: run_with_env.sh <script_name>"
+  exit 1
+fi
+
+TIMESTAMP=`date +%s`
+
+module load cudnn/cuda-9.1/7.1.2
+module load anaconda3/5.0.1
+
+# Note: This makes sure we're running tensorflow with GPU support
+# This is commented out because we only need to do this once
+#pip3 uninstall -y tensorflow
+#pip3 uninstall -y tensorflow-gpu
+#pip3 install --user tensorflow-gpu
+
+python test_gpu_support.py
+
+if [[ "$?" -ne 0 ]]; then
+  echo "GPU test failed. Exiting."
+  exit 1
+fi
+
+bash "$1" "$TIMESTAMP"
+
