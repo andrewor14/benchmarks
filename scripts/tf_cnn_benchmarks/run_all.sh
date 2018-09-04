94106d4706f61c31b277cb20856d5a341d4ad49a
andrewor14 <andrewor14@gmail.com>
2018-09-04 09:01:00 -0400

    Use run tags to separate experiments

--- scripts/tf_cnn_benchmarks/run_all.sh
+++ scripts/tf_cnn_benchmarks/run_all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RUN_TAG="benchmark"
+if [[ -n "$1" ]]; then
+  RUN_TAG="$RUN_TAG-$1"
+fi
+export RUN_TAG
+
 KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="ksync" sbatch slurm_run_benchmark.sh
