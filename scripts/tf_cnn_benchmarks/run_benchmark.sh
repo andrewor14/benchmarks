2ab34818c05f479d85411dca4aa0e89667bc1d2d
andrewor14 <andrewor14@gmail.com>
2018-09-12 06:56:59 -0400

    Run script: allow configuring model and optimizer

--- scripts/tf_cnn_benchmarks/run_benchmark.sh
+++ scripts/tf_cnn_benchmarks/run_benchmark.sh
@@ -3,17 +3,19 @@
 DATA_DIR="/tigress/andrewor/dataset/cifar10-dataset/cifar-10-batches-py"
 TRAIN_DIR="/tigress/andrewor/logs/resnet_cifar10_model_$1"
 NUM_GPUS="${NUM_GPUS:=4}"
+OPTIMIZER="${OPTIMIZER:=ksync}"
 KSYNC_MODE="${KSYNC_MODE:=sync}"
+MODEL="${MODEL:=resnet32}"
 
 python tf_cnn_benchmarks.py\
   --num_gpus="$NUM_GPUS"\
   --batch_size=128\
-  --model=resnet56\
+  --model="$MODEL"\
   --print_training_accuracy=true\
   --num_epochs=100\
   --data_dir="$DATA_DIR"\
   --train_dir="$TRAIN_DIR"\
-  --optimizer=ksync\
+  --optimizer="$OPTIMIZER"\
   --ksync_num_replicas=4\
   --ksync_scaling_duration=6500\
   --ksync_mode="$KSYNC_MODE"\
