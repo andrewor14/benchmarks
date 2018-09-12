8b3febb59bb36ae6649bded986cac10d18b3ab60
andrewor14 <andrewor14@gmail.com>
2018-09-12 06:56:59 -0400

    Run script: allow configuring model and optimizer

--- scripts/tf_cnn_benchmarks/run_all.sh
+++ scripts/tf_cnn_benchmarks/run_all.sh
@@ -5,7 +5,9 @@ if [[ -n "$1" ]]; then
   RUN_TAG="$RUN_TAG-$1"
 fi
 export RUN_TAG
+export MODEL="resnet32"
 
+OPTIMIZER="momentum" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="ksync" sbatch slurm_run_benchmark.sh
