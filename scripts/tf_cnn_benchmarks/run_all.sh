540b760b834c17f03d17d8bd20cbd26c7834874a
andrewor14 <andrewor14@gmail.com>
2018-09-18 16:08:37 -0400

    Set up script to run with cross_replica_sync

--- scripts/tf_cnn_benchmarks/run_all.sh
+++ scripts/tf_cnn_benchmarks/run_all.sh
@@ -7,8 +7,9 @@ fi
 export RUN_TAG
 export MODEL="resnet32"
 
-OPTIMIZER="momentum" sbatch slurm_run_benchmark.sh
+OPTIMIZER="momentum" CROSS_REPLICA_SYNC="false" sbatch slurm_run_benchmark.sh
+OPTIMIZER="momentum" CROSS_REPLICA_SYNC="true" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
-KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
-KSYNC_MODE="ksync" sbatch slurm_run_benchmark.sh
+#KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
+#KSYNC_MODE="ksync" sbatch slurm_run_benchmark.sh
 
--- scripts/tf_cnn_benchmarks/run_benchmark.sh
+++ scripts/tf_cnn_benchmarks/run_benchmark.sh
@@ -6,6 +6,7 @@ NUM_GPUS="${NUM_GPUS:=4}"
 DEVICE="${DEVICE:=gpu}"
 DATA_FORMAT="${DATA_FORMAT:=NCHW}"
 OPTIMIZER="${OPTIMIZER:=ksync}"
+CROSS_REPLICA_SYNC="${CROSS_REPLICA_SYNC:=false}"
 KSYNC_MODE="${KSYNC_MODE:=sync}"
 MODEL="${MODEL:=resnet32}"
 
@@ -26,5 +27,5 @@ python tf_cnn_benchmarks.py\
   --ksync_num_replicas=4\
   --ksync_scaling_duration=6500\
   --ksync_mode="$KSYNC_MODE"\
-  --cross_replica_sync=false
+  --cross_replica_sync="$CROSS_REPLICA_SYNC"
 
