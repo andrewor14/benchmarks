f2b7b8588856b7bd10461b519fccc159dafc1397
andrewor14 <andrewor14@gmail.com>
2018-09-18 15:58:35 -0400

    Remove "mysync" (original SyncReplicasOptimizer)

--- scripts/tf_cnn_benchmarks/run_all.sh
+++ scripts/tf_cnn_benchmarks/run_all.sh
@@ -7,7 +7,6 @@ fi
 export RUN_TAG
 export MODEL="resnet32"
 
-OPTIMIZER="mysync" sbatch slurm_run_benchmark.sh
 OPTIMIZER="momentum" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
 KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
