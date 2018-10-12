c65e518b28a904b63f9be215a9e425ebf70e91b8
andrewor14 <andrewor14@gmail.com>
2018-10-12 15:02:23 -0400

    Set up local mode experiment (1 ps + 1 worker)

--- scripts/tf_cnn_benchmarks/run_all.sh
+++ scripts/tf_cnn_benchmarks/run_all.sh
@@ -6,9 +6,9 @@ if [[ -n "$1" ]]; then
 fi
 export RUN_TAG
 
-OPTIMIZER="momentum" CROSS_REPLICA_SYNC="false" sbatch slurm_run_benchmark.sh
-OPTIMIZER="momentum" CROSS_REPLICA_SYNC="true" sbatch slurm_run_benchmark.sh
-KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
+OPTIMIZER="momentum" CROSS_REPLICA_SYNC="false" sbatch slurm_run_benchmark_local.sh
+#OPTIMIZER="momentum" CROSS_REPLICA_SYNC="true" sbatch slurm_run_benchmark.sh
+#KSYNC_MODE="sync" sbatch slurm_run_benchmark.sh
 #KSYNC_MODE="async" sbatch slurm_run_benchmark.sh
 #KSYNC_MODE="ksync" sbatch slurm_run_benchmark.sh
 
